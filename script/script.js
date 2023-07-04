bea35dc606debab5bf16f50f7d1b029b9ce954f4
OnePunchDev <45498211+OnePunchDev@users.noreply.github.com>
2023-07-04 09:30:56 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,9 +1,9 @@
 const mainDiv = document.getElementById("mainDiv");
 const testText = document.CreateElement("p");
 
-mainDiv.appendChild(testText);
-
 const myName="Davy";
 testText.innerHTML = myName;
+
+mainDiv.appendChild(testText);
 
 
